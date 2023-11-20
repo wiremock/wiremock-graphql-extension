d12610b3e80cee67cd62eb9c058593c9243ea08d
nilwurtz <apollo.oth@gmail.com>
2023-11-21 01:23:54 +0900

    :green_heart: test: added first testcontainer test

--- wiremock-graphql-extension/src/test/kotlin/io/github/nilwurtz/integration/ContainerTest.kt
+++ wiremock-graphql-extension/src/test/kotlin/io/github/nilwurtz/integration/ContainerTest.kt
@@ -1,12 +1,34 @@
 package io.github.nilwurtz.integration
 
-import org.junit.jupiter.api.Assertions
+import com.github.tomakehurst.wiremock.client.WireMock
+import io.github.nilwurtz.GraphqlBodyMatcher
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.net.http.HttpClient
 
 class ContainerTest : BaseContainerTest() {
 
     @Test
     fun `should start wiremock container`() {
-        Assertions.assertTrue(wireMockContainer.isRunning)
+        assertTrue(wireMockContainer.isRunning)
+    }
+
+    @Test
+    fun testMatch() {
+        WireMock(wireMockContainer.port).register(
+            WireMock.post(WireMock.urlEqualTo("/graphql"))
+                .andMatching(GraphqlBodyMatcher.extensionName, GraphqlBodyMatcher.withRequest("""{"query": "query { name id }"}"""))
+                .willReturn(WireMock.aResponse().withStatus(200))
+        )
+
+        val client = HttpClient.newHttpClient()
+        val request = java.net.http.HttpRequest.newBuilder()
+            .uri(java.net.URI.create("${wireMockContainer.baseUrl}/graphql"))
+            .POST(java.net.http.HttpRequest.BodyPublishers.ofString("""{"query": "query { id name }"}"""))
+            .build().let { client.send(it, java.net.http.HttpResponse.BodyHandlers.ofString()) }
+
+        assertEquals(200, request.statusCode())
+
     }
 }
